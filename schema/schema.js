415a734a9619b1adb191992a7d8dfad362d08d13
Jase Snider <jeff.snider@jaseone.com>
2019-04-01 23:17:11 -0500

    added nodemon. added json-server.

--- schema/schema.js
+++ schema/schema.js
@@ -1,13 +1,8 @@
 const graphql = require("graphql");
 const _ = require("lodash");
+const axios = require("axios");
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-
-const users = [
-  { id: "23", firstName: "Bill", age: 14 },
-  { id: "11", firstName: "Sam", age: 24 },
-  { id: "5", firstName: "Hailey", age: 50 }
-];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -25,7 +20,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data);
       }
     }
   }
